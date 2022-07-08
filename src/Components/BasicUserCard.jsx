ac2c1bb1c0331466a535e638a7374836516d38ed
adnankhateeb <adnankhateeb21@gmail.com>
2022-07-08 15:07:54 +0530

    rick and morty- infinite scroll

--- src/Components/BasicUserCard.jsx
+++ src/Components/BasicUserCard.jsx
@@ -5,11 +5,26 @@ import { MdCircle } from "react-icons/md";
 import { ResultsContext } from "../Contexts/ResultsContext";
 import "./styles/basicUserCards.css";
 import DetailsUserCard from "./DetailsUserCard";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useEffect } from "react";
+import axios from "axios";
 
 const BasicUserCard = () => {
-   const { results } = useContext(ResultsContext);
+   const { results, fetchData, total, setTotal, setResults } =
+      useContext(ResultsContext);
    const [showModal, setShowModal] = useState(false);
    const [selectedCard, setSelectedCard] = useState({});
+
+   useEffect(() => {
+      axios
+         .get(
+            `https://rickandmortyapi.com/api/character/?name=&page=1`
+         )
+         .then((data) => {
+            setResults(data.data.results);
+            setTotal(data.data.info.count);
+         });
+   }, []);
 
    const runFunctions = (e) => {
       setSelectedCard(e);
@@ -42,29 +57,40 @@ const BasicUserCard = () => {
                checkColor={checkColor}
             />
          ) : null}
-         <div className="basicUserCard">
-            {results.map((e) => (
-               <div
-                  className="individualCard"
-                  key={e.id}
-                  onClick={() => runFunctions(e)}
-               >
-                  <div className="left">
-                     <img src={e.image} alt="char" />
-                     <p>{e.name}</p>
+
+         <InfiniteScroll
+            dataLength={results.length} //This is important field to render the next data
+            next={fetchData}
+            hasMore={results.length <= total}
+         >
+            <div className="basicUserCard">
+               {results.map((e) => (
+                  <div
+                     className="individualCard"
+                     key={e.id}
+                     onClick={() => runFunctions(e)}
+                  >
+                     <div className="left">
+                        <img src={e.image} alt="char" />
+                        <p>{e.name}</p>
+                     </div>
+                     <div className="right">
+                        <MdCircle
+                           style={{
+                              color: checkColor(e.status),
+                              fontSize: "8px",
+                           }}
+                        />
+                        <span style={{ marginLeft: "3%", marginRight: "1%" }}>
+                           {e.status.charAt(0).toUpperCase() +
+                              e.status.slice(1)}
+                        </span>{" "}
+                        - <span style={{ marginLeft: "1%" }}>{e.species}</span>
+                     </div>
                   </div>
-                  <div className="right">
-                     <MdCircle
-                        style={{ color: checkColor(e.status), fontSize: "8px" }}
-                     />
-                     <span style={{ marginLeft: "3%", marginRight: "1%" }}>
-                        {e.status.charAt(0).toUpperCase() + e.status.slice(1)}
-                     </span>{" "}
-                     - <span style={{ marginLeft: "1%" }}>{e.species}</span>
-                  </div>
-               </div>
-            ))}
-         </div>
+               ))}
+            </div>
+         </InfiniteScroll>
       </>
    );
 };
--- src/Contexts/ResultsContext.jsx
+++ src/Contexts/ResultsContext.jsx
@@ -8,21 +8,44 @@ export const ResultsContext = createContext();
 export const ResultsContextProvider = ({ children }) => {
    const [results, setResults] = useState([]);
    const timeout = useRef();
+   const [page, setPage] = useState(1);
+   const [query, setQuery] = useState("rick");
+   const [total, setTotal] = useState(0);
+
+   const fetchData = () => {
+      setPage(page + 1);
+      axios
+         .get(
+            `https://rickandmortyapi.com/api/character/?name=${query}&page=${page}`
+         )
+         .then((data) => {
+            setResults(results.concat(data.data.results));
+            console.log(results.length)
+            console.log(total);
+         })
+   };
+
 
    const handleChange = (e) => {
+      setQuery(e.target.value);
       clearTimeout(timeout.current);
+      setResults([]);
+      console.log(query)
 
       timeout.current = setTimeout(() => {
          axios
             .get(
-               `https://rickandmortyapi.com/api/character/?name=${e.target.value}&page=1`
+               `https://rickandmortyapi.com/api/character/?name=${query}&page=${page}`
             )
-            .then((data) => setResults(data.data.results)).then(console.log(results));
+            .then((data) => {
+               setResults(data.data.results);
+               setTotal(data.data.info.count);
+            });
       }, 1000);
    };
 
    return (
-      <ResultsContext.Provider value={{ results, handleChange }}>
+      <ResultsContext.Provider value={{ results, handleChange, fetchData, total, setResults, setTotal }}>
          {children}
       </ResultsContext.Provider>
    );
